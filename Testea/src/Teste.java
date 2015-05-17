7750777b6601c43aa349644f82679ca63fbb17ec
sandrobahia8 <sandrobahia8@gmail.com>
2015-05-17 19:31:51 -0300

    Adicionando método get e set.

--- Testea/src/Teste.java
+++ Testea/src/Teste.java
@@ -1,11 +1,21 @@
 
 public class Teste {
 
-	/**
-	 * @param args
-	 */
+	public String nome;
+		
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public static void main(String[] args) {
+		Teste teste = new Teste();
 		System.out.println("Teste 1");
+		teste.setNome("Alessandro");
+		System.out.print(teste.getNome());
 
 	}
 
